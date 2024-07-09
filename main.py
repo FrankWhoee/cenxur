318fb0de1cf2722264efa0a087a0102f59f7ae2f
FrankWhoee <huifrank@live.com>
2024-07-09 01:03:15 -0700

    Add db size check

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from threading import Timer
 from dotenv import load_dotenv
 import os
 from utils import create_env_template
-
+import random
 
 # CONSTANTS
 VOTING_TIME = 600 # How long before voting closes on flagged messages in seconds
@@ -12,6 +12,8 @@ AFFIRMATIVE_EMOJI = "🟩"
 NEGATIVE_EMOJI = "🟥"
 FLAG_EMOJI = "🚩"
 DATABASE_NAME = "data.db"
+FLAG_PROBABILITY = 0.1 # Probability that a message gets randomly flagged for data collection
+PREFIX = "!"
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -70,20 +72,24 @@ def add_message_to_db(message):
     con.commit()
     con.close()
 
+def get_table_size():
+    con = sqlite3.connect(DATABASE_NAME)
+    cur = con.cursor()
+    res = cur.execute("SELECT COUNT(*) FROM messages;")
+    return res.fetchone()[0]
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    # react to message with a red flag
-    await message.add_reaction(FLAG_EMOJI)
-    await message.add_reaction(AFFIRMATIVE_EMOJI)
-    await message.add_reaction(NEGATIVE_EMOJI)
-    Timer(VOTING_TIME, add_message_to_db, args=[message]).start()
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith(PREFIX + "dbsize"):
+        await message.channel.send(get_table_size())
+    elif random.random() <= FLAG_PROBABILITY:
+        await message.add_reaction(FLAG_EMOJI)
+        await message.add_reaction(AFFIRMATIVE_EMOJI)
+        await message.add_reaction(NEGATIVE_EMOJI)
+        Timer(VOTING_TIME, add_message_to_db, args=[message]).start()
 
 ENVIRONMENT = os.environ['ENVIRONMENT']
 PROD_KEY = os.environ["PROD_KEY"]
